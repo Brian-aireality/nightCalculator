c844b8bdf139a56553160aa5a05ceeecbd6a6e52
Student <example@example.com>
2019-11-06 19:48:50 -0800

    add initial files and js script

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+function subtract(number1, number2) {
+  return number1 - number2;
+};
+
+function multiply(number1, number2) {
+  return number1 * number2;
+};
+
+function divide(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(divide(number1, number2));
